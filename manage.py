f493602cb348bacc16ca4e26c56c9be6b1b3d38c
shivprasadd <shivprasad.dahiphale@ap.jll.com>
2022-10-05 15:23:50 +0530

    Test

--- manage.py
+++ manage.py
@@ -6,7 +6,6 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ["DEVELOPMENT_MODE"] = "True"
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_app.settings')
     try:
         from django.core.management import execute_from_command_line
